d065a494fbc24ee9e3e7e36aa5a2ce8b91569cd8
Ukaza Perdana <hello@ukazap.space>
2020-02-25 16:07:53 +0800

    Alias

--- x/nameservice/alias.go
+++ x/nameservice/alias.go
@@ -18,17 +18,19 @@ const (
 
 var (
 	// functions aliases
-	NewKeeper           = keeper.NewKeeper
-	NewQuerier          = keeper.NewQuerier
-	RegisterCodec       = types.RegisterCodec
-	NewGenesisState     = types.NewGenesisState
-	DefaultGenesisState = types.DefaultGenesisState
-	ValidateGenesis     = types.ValidateGenesis
-	// TODO: Fill out function aliases
+    NewKeeper           = keeper.NewKeeper
+    NewQuerier          = keeper.NewQuerier
+    RegisterCodec       = types.RegisterCodec
+    NewGenesisState     = types.NewGenesisState
+    DefaultGenesisState = types.DefaultGenesisState
+    ValidateGenesis     = types.ValidateGenesis
+	NewMsgBuyName       = types.NewMsgBuyName
+    NewMsgSetName       = types.NewMsgSetName
+    NewMsgDeleteName    = types.NewMsgDeleteName
+    NewWhois            = types.NewWhois
 
 	// variable aliases
-	ModuleCdc     = types.ModuleCdc
-	// TODO: Fill out variable aliases
+    ModuleCdc           = types.ModuleCdc
 )
 
 type (
@@ -36,5 +38,10 @@ type (
 	GenesisState = types.GenesisState
 	Params       = types.Params
 
-	// TODO: Fill out module types
+	MsgSetName      = types.MsgSetName
+    MsgBuyName      = types.MsgBuyName
+    MsgDeleteName   = types.MsgDeleteName
+    QueryResResolve = types.QueryResResolve
+    QueryResNames   = types.QueryResNames
+    Whois           = types.Whois
 )
