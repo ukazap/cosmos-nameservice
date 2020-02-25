c245c4a1157df5e11f1e5f53a8d8c808ea4c5242
Ukaza Perdana <hello@ukazap.space>
2020-02-25 16:11:05 +0800

    Codec

--- x/nameservice/internal/types/codec.go
+++ x/nameservice/internal/types/codec.go
@@ -6,7 +6,9 @@ import (
 
 // RegisterCodec registers concrete types on codec
 func RegisterCodec(cdc *codec.Codec) {
-	// TODO: Register the modules msgs
+	cdc.RegisterConcrete(MsgSetName{}, "nameservice/SetName", nil)
+    cdc.RegisterConcrete(MsgBuyName{}, "nameservice/BuyName", nil)
+    cdc.RegisterConcrete(MsgDeleteName{}, "nameservice/DeleteName", nil)
 }
 
 // ModuleCdc defines the module codec
