2e5d6dd1c4703b0e54d2003b3dfb587c54105c31
Ukaza Perdana <hello@ukazap.space>
2020-02-25 14:46:34 +0800

    Define messages and handlers
    - SetName
    - BuyName
    - DeleteName

--- x/nameservice/handler.go
+++ x/nameservice/handler.go
@@ -2,43 +2,67 @@ package nameservice
 
 import (
 	"fmt"
-
+    "github.com/ukazap/cosmos-nameservice/x/nameservice/internal/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-// NewHandler creates an sdk.Handler for all the nameservice type messages
-func NewHandler(k Keeper) sdk.Handler {
-	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
-		ctx = ctx.WithEventManager(sdk.NewEventManager())
-		switch msg := msg.(type) {
-		// TODO: Define your msg cases
-		// 
-		//Example:
-		// case Msg<Action>:
-		// 	return handleMsg<Action>(ctx, k, msg)
-		default:
-			errMsg := fmt.Sprintf("unrecognized %s message type: %T", ModuleName,  msg)
-			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
-		}
-	}
+// NewHandler returns a handler for "nameservice" type messages.
+func NewHandler(keeper Keeper) sdk.Handler {
+    return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
+        switch msg := msg.(type) {
+        case MsgSetName:
+            return handleMsgSetName(ctx, keeper, msg)
+        case MsgBuyName:
+            return handleMsgBuyName(ctx, keeper, msg)
+        case MsgDeleteName:
+            return handleMsgDeleteName(ctx, keeper, msg)
+        default:
+            return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, fmt.Sprintf("Unrecognized nameservice Msg type: %v", msg.Type()))
+        }
+    }
 }
 
-// handde<Action> does x
-func handleMsg<Action>(ctx sdk.Context, k Keeper, msg Msg<Action>) (*sdk.Result, error) {
-	err := k.<Action>(ctx, msg.ValidatorAddr)
-	if err != nil {
-		return nil, err
-	}
+// Handle a message to set name
+func handleMsgSetName(ctx sdk.Context, keeper Keeper, msg MsgSetName) (*sdk.Result, error) {
+    if !msg.Owner.Equals(keeper.GetOwner(ctx, msg.Name)) { // Checks if the the msg sender is the same as the current owner
+        return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Incorrect Owner") // If not, throw an error
+    }
+    keeper.SetName(ctx, msg.Name, msg.Value) // If so, set the name to the value specified in the msg.
+    return &sdk.Result{}, nil                // return
+}
 
-	// TODO: Define your msg events
-	ctx.EventManager().EmitEvent(
-		sdk.NewEvent(
-			sdk.EventTypeMessage,
-			sdk.NewAttribute(sdk.AttributeKeyModule, AttributeValueCategory),
-			sdk.NewAttribute(sdk.AttributeKeySender, msg.ValidatorAddr.String()),
-		),
-	)
+// Handle a message to buy name
+func handleMsgBuyName(ctx sdk.Context, keeper Keeper, msg MsgBuyName) (*sdk.Result, error) {
+    // Checks if the the bid price is greater than the price paid by the current owner
+    if keeper.GetPrice(ctx, msg.Name).IsAllGT(msg.Bid) {
+        return nil, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "Bid not high enough") // If not, throw an error
+    }
+    if keeper.HasOwner(ctx, msg.Name) {
+        err := keeper.CoinKeeper.SendCoins(ctx, msg.Buyer, keeper.GetOwner(ctx, msg.Name), msg.Bid)
+        if err != nil {
+            return nil, err
+        }
+    } else {
+        _, err := keeper.CoinKeeper.SubtractCoins(ctx, msg.Buyer, msg.Bid) // If so, deduct the Bid amount from the sender
+        if err != nil {
+            return nil, err
+        }
+    }
+    keeper.SetOwner(ctx, msg.Name, msg.Buyer)
+    keeper.SetPrice(ctx, msg.Name, msg.Bid)
+    return &sdk.Result{}, nil
+}
 
-	return &sdk.Result{Events: ctx.EventManager().Events()}, nil
+// Handle a message to delete name
+func handleMsgDeleteName(ctx sdk.Context, keeper Keeper, msg MsgDeleteName) (*sdk.Result, error) {
+    if !keeper.IsNamePresent(ctx, msg.Name) {
+        return nil, sdkerrors.Wrap(types.ErrNameDoesNotExist, msg.Name)
+    }
+    if !msg.Owner.Equals(keeper.GetOwner(ctx, msg.Name)) {
+        return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Incorrect Owner")
+    }
+
+    keeper.DeleteWhois(ctx, msg.Name)
+    return &sdk.Result{}, nil
 }
--- x/nameservice/internal/types/msg.go
+++ x/nameservice/internal/types/msg.go
@@ -5,43 +5,142 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-// TODO: Describe your actions, these will implment the interface of `sdk.Msg`
-/*
-verify interface at compile time
-var _ sdk.Msg = &Msg<Action>{}
+const RouterKey = ModuleName // this was defined in your key.go file
 
-Msg<Action> - struct for unjailing jailed validator
-type Msg<Action> struct {
-	ValidatorAddr sdk.ValAddress `json:"address" yaml:"address"` // address of the validator operator
+
+
+
+// MsgSetName defines a SetName message
+type MsgSetName struct {
+    Name  string         `json:"name"`
+    Value string         `json:"value"`
+    Owner sdk.AccAddress `json:"owner"`
 }
 
-NewMsg<Action> creates a new Msg<Action> instance
-func NewMsg<Action>(validatorAddr sdk.ValAddress) Msg<Action> {
-	return Msg<Action>{
-		ValidatorAddr: validatorAddr,
-	}
+// NewMsgSetName is a constructor function for MsgSetName
+func NewMsgSetName(name string, value string, owner sdk.AccAddress) MsgSetName {
+    return MsgSetName{
+        Name:  name,
+        Value: value,
+        Owner: owner,
+    }
 }
 
-const <action>Const = "<action>"
+// Route should return the name of the module
+func (msg MsgSetName) Route() string { return RouterKey }
 
-// nolint
-func (msg Msg<Action>) Route() string { return RouterKey }
-func (msg Msg<Action>) Type() string  { return <action>Const }
-func (msg Msg<Action>) GetSigners() []sdk.AccAddress {
-	return []sdk.AccAddress{sdk.AccAddress(msg.ValidatorAddr)}
+// Type should return the action
+func (msg MsgSetName) Type() string { return "set_name" }
+
+// ValidateBasic runs stateless checks on the message
+func (msg MsgSetName) ValidateBasic() error {
+    if msg.Owner.Empty() {
+        return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, msg.Owner.String())
+    }
+    if len(msg.Name) == 0 || len(msg.Value) == 0 {
+        return sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "Name and/or Value cannot be empty")
+    }
+    return nil
 }
 
-GetSignBytes gets the bytes for the message signer to sign on
-func (msg Msg<Action>) GetSignBytes() []byte {
-	bz := ModuleCdc.MustMarshalJSON(msg)
-	return sdk.MustSortJSON(bz)
+// GetSignBytes encodes the message for signing
+func (msg MsgSetName) GetSignBytes() []byte {
+    return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(msg))
 }
 
-ValidateBasic validity check for the AnteHandler
-func (msg Msg<Action>) ValidateBasic() error {
-	if msg.ValidatorAddr.Empty() {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "missing validator address"
-	}
-	return nil
+// GetSigners defines whose signature is required
+func (msg MsgSetName) GetSigners() []sdk.AccAddress {
+    return []sdk.AccAddress{msg.Owner}
 }
-*/
+
+
+
+
+// MsgBuyName defines the BuyName message
+type MsgBuyName struct {
+    Name  string         `json:"name"`
+    Bid   sdk.Coins      `json:"bid"`
+    Buyer sdk.AccAddress `json:"buyer"`
+}
+
+// NewMsgBuyName is the constructor function for MsgBuyName
+func NewMsgBuyName(name string, bid sdk.Coins, buyer sdk.AccAddress) MsgBuyName {
+    return MsgBuyName{
+        Name:  name,
+        Bid:   bid,
+        Buyer: buyer,
+    }
+}
+
+// Route should return the name of the module
+func (msg MsgBuyName) Route() string { return RouterKey }
+
+// Type should return the action
+func (msg MsgBuyName) Type() string { return "buy_name" }
+
+// ValidateBasic runs stateless checks on the message
+func (msg MsgBuyName) ValidateBasic() error {
+    if msg.Buyer.Empty() {
+        return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, msg.Buyer.String())
+    }
+    if len(msg.Name) == 0 {
+        return sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "Name cannot be empty")
+    }
+    if !msg.Bid.IsAllPositive() {
+        return sdkerrors.ErrInsufficientFunds
+    return nil
+}
+
+// GetSignBytes encodes the message for signing
+func (msg MsgBuyName) GetSignBytes() []byte {
+    return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(msg))
+}
+
+// GetSigners defines whose signature is required
+func (msg MsgBuyName) GetSigners() []sdk.AccAddress {
+    return []sdk.AccAddress{msg.Buyer}
+}
+
+
+
+
+// MsgDeleteName defines a DeleteName message
+type MsgDeleteName struct {
+    Name  string         `json:"name"`
+    Owner sdk.AccAddress `json:"owner"`
+}
+
+// NewMsgDeleteName is a constructor function for MsgDeleteName
+func NewMsgDeleteName(name string, owner sdk.AccAddress) MsgDeleteName {
+    return MsgDeleteName{
+        Name:  name,
+        Owner: owner,
+    }
+}
+
+// Route should return the name of the module
+func (msg MsgDeleteName) Route() string { return RouterKey }
+
+// Type should return the action
+func (msg MsgDeleteName) Type() string { return "delete_name" }
+
+// ValidateBasic runs stateless checks on the message
+func (msg MsgDeleteName) ValidateBasic() error {
+    if msg.Owner.Empty() {
+        return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, msg.Owner.String())
+    }
+    if len(msg.Name) == 0 {
+        return sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "Name cannot be empty")
+    }
+    return nil
+}
+
+// GetSignBytes encodes the message for signing
+func (msg MsgDeleteName) GetSignBytes() []byte {
+    return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(msg))
+}
+
+// GetSigners defines whose signature is required
+func (msg MsgDeleteName) GetSigners() []sdk.AccAddress {
+    return []sdk.AccAddress{msg.Owner}
+}
