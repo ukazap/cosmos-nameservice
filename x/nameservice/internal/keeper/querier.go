3047ebfd242346ba6a1c83569917294a7b65330a
Ukaza Perdana <hello@ukazap.space>
2020-02-25 16:00:07 +0800

    Querier

--- x/nameservice/internal/keeper/querier.go
+++ x/nameservice/internal/keeper/querier.go
@@ -12,29 +12,70 @@ import (
 	"github.com/ukazap/cosmos-nameservice/x/nameservice/internal/types"
 )
 
-// NewQuerier creates a new querier for nameservice clients.
-func NewQuerier(k Keeper) sdk.Querier {
-	return func(ctx sdk.Context, path []string, req abci.RequestQuery) ([]byte, error) {
-		switch path[0] {
-		case types.QueryParams:
-			return queryParams(ctx, k)
-			// TODO: Put the modules query routes
-		default:
-			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "unknown nameservice query endpoint")
-		}
-	}
+// query endpoints supported by the nameservice Querier
+const (
+    QueryResolve = "resolve"
+    QueryWhois   = "whois"
+    QueryNames   = "names"
+)
+
+// NewQuerier is the module level router for state queries
+func NewQuerier(keeper Keeper) sdk.Querier {
+    return func(ctx sdk.Context, path []string, req abci.RequestQuery) (res []byte, err error) {
+        switch path[0] {
+        case QueryResolve:
+            return queryResolve(ctx, path[1:], req, keeper)
+        case QueryWhois:
+            return queryWhois(ctx, path[1:], req, keeper)
+        case QueryNames:
+            return queryNames(ctx, req, keeper)
+        default:
+            return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "unknown nameservice query endpoint")
+        }
+    }
 }
 
-func queryParams(ctx sdk.Context, k Keeper) ([]byte, error) {
-	params := k.GetParams(ctx)
+// nolint: unparam
+func queryResolve(ctx sdk.Context, path []string, req abci.RequestQuery, keeper Keeper) ([]byte, error) {
+    value := keeper.ResolveName(ctx, path[0])
 
-	res, err := codec.MarshalJSONIndent(types.ModuleCdc, params)
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
-	}
+    if value == "" {
+        return []byte{}, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "could not resolve name")
+    }
 
-	return res, nil
+    res, err := codec.MarshalJSONIndent(keeper.cdc, types.QueryResResolve{Value: value})
+    if err != nil {
+        return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+    }
+
+    return res, nil
 }
 
-// TODO: Add the modules query functions
-// They will be similar to the above one: queryParams()
+// nolint: unparam
+func queryWhois(ctx sdk.Context, path []string, req abci.RequestQuery, keeper Keeper) ([]byte, error) {
+    whois := keeper.GetWhois(ctx, path[0])
+
+    res, err := codec.MarshalJSONIndent(keeper.cdc, whois)
+    if err != nil {
+        return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+    }
+
+    return res, nil
+}
+
+func queryNames(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) ([]byte, error) {
+    var namesList types.QueryResNames
+
+    iterator := keeper.GetNamesIterator(ctx)
+
+    for ; iterator.Valid(); iterator.Next() {
+        namesList = append(namesList, string(iterator.Key()))
+    }
+
+    res, err := codec.MarshalJSONIndent(keeper.cdc, namesList)
+    if err != nil {
+        return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+    }
+
+    return res, nil
+}
--- x/nameservice/internal/types/querier.go
+++ x/nameservice/internal/types/querier.go
@@ -1,22 +1,21 @@
 package types
 
+import "strings"
 
-// Query endpoints supported by the nameservice querier
-const (
-	// TODO: Describe query parameters, update <action> with your query
-	// Query<Action>    = "<action>"
-)
-
-/* 
-Below you will be able how to set your own queries:
-
-
-// QueryResList Queries Result Payload for a query
-type QueryResList []string
-
-// implement fmt.Stringer
-func (n QueryResList) String() string {
-	return strings.Join(n[:], "\n")
+// QueryResResolve Queries Result Payload for a resolve query
+type QueryResResolve struct {
+    Value string `json:"value"`
 }
 
-*/
+// implement fmt.Stringer
+func (r QueryResResolve) String() string {
+    return r.Value
+}
+
+// QueryResNames Queries Result Payload for a names query
+type QueryResNames []string
+
+// implement fmt.Stringer
+func (n QueryResNames) String() string {
+    return strings.Join(n[:], "\n")
+}
